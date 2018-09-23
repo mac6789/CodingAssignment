52d95d2eacedc373395aaa991cf3f5efbbbaebc5
mac6789 <43511589+mac6789@users.noreply.github.com>
2018-09-23 15:38:00 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# Specify input file names in front of variables "input_transaction_path" and "start_day_position_path"
+
+input_files= {'input_transaction_path': 'C:\\Users\\Admin\\Downloads\\topurgentcodingassignment\\Input_Transactions.txt',
+         'start_day_position_path': 'C:\\Users\\Admin\\Downloads\\topurgentcodingassignment\\Input_StartOfDay_Positions.txt' }
+
+# Please specify the path where you want to write your end of the day position output file
+
+output_file={'writepath' :'C:\\Users\\Admin\\Downloads\\topurgentcodingassignment\\Output_EndOfDay_Positions.txt'}
